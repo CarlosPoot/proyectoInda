b5cec7fc710aa2aa61eb65de206e590e1ec30d15
dzib_101@hotmail.com <dzib_101@hotmail.com>
2020-07-31 00:51:02 -0500

    cambios

--- controlador/AltaUsuarioController.js
+++ controlador/AltaUsuarioController.js
@@ -11,7 +11,40 @@ app.controller('altaUsuarioController', function($scope, $q, $timeout, servicioA
             mostrarMensajeModal("Datos incompletos", "Ingrese el número de cliente por favor");
             return false;
         }else if( !$scope.cliente.oficina ){
-            mostrarMensajeModal("Datos incompletos", "Ingrese el número de cliente por favor");
+            mostrarMensajeModal("Datos incompletos", "Ingrese el número del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.nombre ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el nombre del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.apellido ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el apellido del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.nss ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el nss del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.curp ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el curp del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.afore ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el afore del cliente por favor");
+            return false;
+        }else if( !$scope.cliente.sc ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el NSS de cliente por favor");
+            return false;
+        }else if( !$scope.cliente.sd ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese las semnas descontadas de cliente por favor");
+            return false;
+        }else if( !$scope.cliente.fb ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese la fecha de baja de cliente por favor");
+            return false;
+        }else if( !$scope.cliente.sbc ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese el salario base cotizado por favor");
+            return false;
+        }else if( !$scope.cliente.alta ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese la fecha del alta por favor");
+            return false;
+        }else if( !$scope.cliente.comentarios ){
+            mostrarMensajeModal("Datos incompletos", "Ingrese comentario por favor");
             return false;
         }
         
